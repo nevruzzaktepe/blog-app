7a7681815a55bb95638e6d1fd15e8649c791bf6a
Nevruz Aktepe <nevruzaktepe@Nevruz-MacBook-Air.local>
2024-11-18 17:07:10 +0300

    devamı

--- src/app/cozumler/page.js
+++ src/app/cozumler/page.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+const datas = [
+  {
+    id: 1,
+    name: "Çözüm 1",
+  },
+  {
+    id: 2,
+    name: "Çözüm 2",
+  },
+  {
+    id: 3,
+    name: "Çözüm 3",
+  },
+  {
+    id: 4,
+    name: "Çözüm 4",
+  },
+];
 
 const Cozumler = () => {
   return (
-    <div>Cozumler</div>
-  )
-}
+    <div className="cozumlerPage">
+      <div className="cozumler">
+        {datas.map((item) => {
+          return (
+            <div key={item.id} className="cozumItem">
+              <Link href={`/cozumler/${item.id}`}>
+              {item.name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Cozumler
+export default Cozumler;
